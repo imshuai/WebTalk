af755bf202298ccd94d6b1d3a05395d86821327e
irislee <iris-me@live.com>
2017-05-01 22:40:59 +0800

    完成ping pong功能

--- main.go
+++ main.go
@@ -2,10 +2,11 @@ package main
 
 import (
 	"errors"
-	"io"
 	"log"
 	"net/http"
+	"time"
 
+	"github.com/gorilla/websocket"
 	"gopkg.in/gin-gonic/gin.v1"
 )
 
@@ -27,21 +28,62 @@ func main() {
 			c.AbortWithError(http.StatusInternalServerError, errors.New("faild to upgrade request to websocket"))
 			return
 		}
-		for {
-			w, err := conn.NextWriter(1)
-			if err != nil {
-				log.Println("unable to get write stream")
-				return
+		conn.SetCloseHandler(func(code int, text string) error {
+			log.Println("user closed connection with code[", code, "] and text[", text, "]")
+			return nil
+		})
+		conn.SetPingHandler(func(appData string) error {
+			conn.WriteControl(websocket.PongMessage, []byte(appData), time.Now().Add(time.Second))
+			return nil
+		})
+		conn.SetPongHandler(func(appData string) error {
+			conn.WriteControl(websocket.PingMessage, []byte(appData), time.Now().Add(time.Second))
+			return nil
+		})
+		isAlive := make(chan bool)
+		go func(conn *websocket.Conn, isAlive chan bool) {
+			t := time.NewTicker(time.Second * 10)
+			for {
+				select {
+				case <-t.C:
+					select {
+					case <-isAlive: //这里判断chan中是否有数据，保证之后读取的时候是空的
+					case <-time.NewTicker(time.Millisecond * 100).C:
+					}
+					conn.WriteMessage(websocket.TextMessage, []byte("control:ping"))
+					select {
+					case i := <-isAlive:
+						if i {
+							log.Println("user is still alive, keep connection")
+						} else {
+							log.Println("user is not alive, closing the connection")
+							conn.Close()
+							return
+						}
+					case <-t.C:
+						log.Println("user is not alive, closing the connection")
+						conn.Close()
+						return
+					}
+				}
 			}
-			messageType, r, err := conn.NextReader()
+		}(conn, isAlive)
+		for {
+			messageType, r, err := conn.ReadMessage()
 			if err != nil {
 				log.Println("recive message fail, err:", err)
 				break
 			}
 			switch messageType {
 			case 1:
-				log.Println("recived a text msg")
-				io.Copy(w, r)
+				if string(r) == "control:ping" {
+					conn.WriteMessage(messageType, []byte("control:pong"))
+				} else if string(r) == "control:pong" {
+					isAlive <- true
+				} else {
+					log.Println("recived a text msg")
+					conn.WriteMessage(messageType, r)
+				}
 			case 2:
 				log.Println("recived a binary msg")
 			case 8:
@@ -52,8 +94,6 @@ func main() {
 				log.Println("recived a pong msg")
 			default:
 				log.Println("recived a unkown msg")
-				conn.Close()
-				break
 			}
 		}
 	})
